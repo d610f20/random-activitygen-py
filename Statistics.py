b3d12b412617d7ac84fbb38085e12f42356f40b9
NicEastvillage <nicoesterby@gmail.com>
2020-03-17 14:47:22 +0100

    Added gitignore and xml parsing example.

---
+++ Statistics.py
@@ -0,0 +1,10 @@
+import xml.etree.ElementTree as ET
+
+if __name__ == '__main__':
+    stats = ET.parse("stats.xml")
+    brackets = stats.find("population").findall("bracket")
+    dankPlaces = ET.SubElement(stats.getroot(), "dankPlaces")
+    ET.SubElement(dankPlaces, "place", {"name": "Aalborg University"})
+    ET.SubElement(dankPlaces, "place", {"name": "Elika's Pizza"})
+    ET.SubElement(dankPlaces, "place", {"name": "Dice'n'Drinks"})
+    stats.write("out/stats2.xml")
