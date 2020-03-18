ea3ca6359b59306f4416181a79f40ab370797225
NicEastvillage <nicoesterby@gmail.com>
2020-03-18 13:37:16 +0100

    [net] Add function to find city centre.

--- randomActivityGen.py
+++ randomActivityGen.py
@@ -12,8 +12,35 @@ Other Options:
     -h, --help                  Show this screen.
     --version                   Show version.
 """
+import os
+import sys
+from typing import Tuple
+
+import numpy as np
 from docopt import docopt
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+import sumolib
+
+
+def find_city_centre(net: sumolib.net.Net) -> Tuple[float, float]:
+    """
+    Finds the city centre; average node coord of all nodes in the net
+    """
+    node_coords = [node.getCoord() for node in net.getNodes()]
+    return float(np.mean([c[0] for c in node_coords])), float(np.mean([c[1] for c in node_coords]))
+
 
 if __name__ == "__main__":
     args = docopt(__doc__, version="RandomActivityGen 0.1")
-    print(f"Arguments: {args}")
+
+    net = sumolib.net.readNet(args["--net-file"])
+
+    centre = find_city_centre(net)
+    print(f"City centre: {centre}")
+
