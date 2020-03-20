9c155c7123d4694786df68d2afbcd01933742bf6
NicEastvillage <nicoesterby@gmail.com>
2020-03-20 09:21:05 +0100

    [city-gates] Write city gates to stats file

--- randomActivityGen.py
+++ randomActivityGen.py
@@ -16,11 +16,11 @@ import math
 import os
 import random
 import sys
-from typing import Tuple, List
+import xml.etree.ElementTree as ET
+from typing import Tuple
 
 import numpy as np
 from docopt import docopt
-import xml.etree.ElementTree as ET
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -39,15 +39,16 @@ def find_city_centre(net: sumolib.net.Net) -> Tuple[float, float]:
     return float(np.mean([c[0] for c in node_coords])), float(np.mean([c[1] for c in node_coords]))
 
 
-
 def setup_city_gates(net: sumolib.net.Net, stats: ET.ElementTree, gate_count: int):
 
-    centre = find_city_centre(net)
+    # Find existing gates to determine how many we need to insert
+    xml_gates = stats.find("cityGates")
+    xml_entrance = xml_gates.findall("entrance")
+    n = gate_count - len(xml_entrance)
+    print(f"Inserting {n} city gates")
 
     # Finds all nodes that are dead ends, i.e. nodes that only have one neighbouring node
     dead_ends = [n for n in net.getNodes() if len(n.getNeighboringNodes()) == 1]
-
-    n = gate_count
 
     # Find n unit vectors pointing in different directions
     # If n = 4 and base_rad = 0 we get the cardinal directions:
@@ -61,17 +62,33 @@ def setup_city_gates(net: sumolib.net.Net, stats: ET.ElementTree, gate_count: in
     rads = [(base_rad + i * tau / n) % tau for i in range(0, n)]
     dirs = [(math.cos(rad), math.sin(rad)) for rad in rads]
 
-    # Find the dead ends furthest in each direction using the dot product and argmax. Those nodes will be our gates
-    gate_indexes = [np.argmax([np.dot(node.getCoord(), dir) for node in dead_ends]) for dir in dirs]
-    # We store the gates in a set to avoid duplicates
-    gates = {dead_ends[i] for i in gate_indexes}
-    print({g.getID() for g in gates})
-    pass
+    for dir in dirs:
+        # Find the dead ends furthest in each direction using the dot product and argmax. Those nodes will be our gates.
+        # Duplicates are possible and no problem. That just means there will be more traffic through that gate.
+        gate_index = int(np.argmax([np.dot(node.getCoord(), dir) for node in dead_ends]))
+        gate = dead_ends[gate_index]
+
+        # Decide proportion of the incoming and outgoing vehicles coming through this gate
+        # These numbers are relatively to the values of the other gates
+        incoming = 1 + random.random()
+        outgoing = 1 + random.random()
+
+        edge = gate.getOutgoing()[0]
+        ET.SubElement(xml_gates, "entrance", attrib={
+            "edge": edge.getID(),
+            "incoming": str(incoming),
+            "outgoing": str(outgoing),
+            "pos": "0"
+        })
 
 
 if __name__ == "__main__":
     args = docopt(__doc__, version="RandomActivityGen 0.1")
 
     net = sumolib.net.readNet(args["--net-file"])
+    stats = ET.parse(args["--stat-file"])
 
-    setup_city_gates(net, None, 5)
+    setup_city_gates(net, stats, 5)
+
+    stats.write(args["--output-file"])
+
