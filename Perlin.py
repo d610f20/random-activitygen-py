df002c039841dad93b06945e8281079dea7a1ee7
cogitantium <falke.cs@gmail.com>
2020-03-20 10:08:00 +0100

    [perlin] Add industry noise application

--- Perlin.py
+++ Perlin.py
@@ -15,9 +15,9 @@ else:
 
 import sumolib
 
-# The 'noise' lib has good resolution until above 10 mil.
-POPULATION_BASE = random.randrange(-1000000, 1000000)
-INDUSTRY_BASE = random.randrange(-1000000, 1000000)
+# The 'noise' lib has good resolution until above 10 mil, but a SIGSEGV is had on values above [-100000, 100000]
+POPULATION_BASE = random.randrange(-100000, 100000)
+INDUSTRY_BASE = random.randrange(-100000, 100000)
 
 
 def get_edge_pair_centroid(coords: list) -> (float, float):
@@ -31,52 +31,55 @@ def get_edge_pair_centroid(coords: list) -> (float, float):
     return x_avg, y_avg
 
 
-def normalise_noise(noise: float) -> float:
+def normalise_noise(noise_val: float) -> float:
     """
     The 'noise' lib returns a value in the range of [-1:1]. The noise value is scaled to the range of [0:1].
-    :param noise: a float [-1:1]
+    :param noise_val: a float [-1:1]
     :return: the noise value scaled to [0:1]
     """
-    return (noise + 1) / 2
+    return (noise_val + 1) / 2
 
 
-def get_perlin_noise(x, y, base) -> float:
+def get_perlin_noise(x: float, y: float, base: float) -> float:
     """
-    TODO: Find sane offset to combat zero-value at (0, 0)
-    :param x:
-    :param y:
-    :return:
+    :param base: offset into noisemap
+    :param x: the sample point for x
+    :param y: the sample point for y
+    :return: a normalised float of the sample in noisemap
     """
     return normalise_noise(noise.pnoise2(x, y, base=base))
 
 
-def get_population_number(net: sumolib.net.Net, edge) -> float:
+def get_population_number(net: sumolib.net.Net, edge, base: float) -> float:
     """
     Returns a Perlin simplex noise at centre of given street
+    :param base: offset into noisemap
     :param net: the SUMO network
     :param edge: the edge ID
     :return: the scaled noise value as float in [0:1]
     """
     x, y = get_edge_pair_centroid(net.getEdge(edge).getShape())
-    return get_perlin_noise(x, y, POPULATION_BASE)
+    return get_perlin_noise(x, y, base)
 
 
-def calculate_network_population(net: sumolib.net.Net, xml: ElementTree):
+def apply_network_noise(net: sumolib.net.Net, xml: ElementTree):
     """
-    Calculate and apply the Perlin noise in [0:1] range for each street
+    Calculate and apply Perlin noise in [0:1] range for each street for population and industry
     :param net: the SUMO network
     :param xml: the statistics XML for the network
     :return:
     """
     for edge in list(map(lambda x: x.getID(), net.getEdges())):
-        pop = get_population_number(net, edge)
-        streets = xml.find("streets").findall("street")
-        for street in streets:
+        population = get_population_number(net, edge, POPULATION_BASE)
+        industry = get_population_number(net, edge, INDUSTRY_BASE)
+
+        for street in xml.find("streets").findall("street"):
             if street.attrib["edge"] == edge:
-                street.set("population", str(pop))
+                street.set("population", str(population))
+                street.set("workPosition", str(industry))
 
 
 def apply_perlin_noise(net: sumolib.net.Net, stats: ET.ElementTree):
     # Calculate and apply Perlin noise for all edges in network to population in statistics
-    print("Writing Perlin noise to population")
-    calculate_network_population(net, stats)
+    print("Writing Perlin noise to population and industry")
+    apply_network_noise(net, stats)
