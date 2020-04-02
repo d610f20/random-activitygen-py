f44bbc592521685d7d7d4eda2c2bda1f47ffe010
Jacob Jenner Rasmussen <jacob@jener.dk>
2020-04-02 09:45:26 +0200

    [bus-stop] Bug fixes
    Fixed dist_function using y instead of x at one point.
    Fixed selection filter poisson disk only filtering against center point
    instead of all points.
    Use the correct function to add elements to all_points. Set doesn't have
    an append function.
    Removed print debug statements.

--- bus.py
+++ bus.py
@@ -9,8 +9,8 @@ def firstn(n, gen):
 
 def dist(point1, point2):
     return math.sqrt(
-        abs(point1[0]-point2[1])**2.0 +
-        abs(point1[1]-point2[1])**2.0)
+        (point1[0]-point2[0])**2.0 +
+        (point1[1]-point2[1])**2.0)
 
 
 def disk_generator(inner_r, outer_r):
@@ -52,7 +52,7 @@ def poision_disk_generator(inner_r, outer_r, seeds, bounds, k=10):
     if bounds[1] > bounds[3]:
         bounds[1], bounds[3] = bounds[3], bounds[1]
 
-    in_bounds = lambda p:  bounds[0] < p[0] and p[0] < bounds[2] and bounds[1] < p[1] and p[1] < bounds[3]
+    in_bounds = lambda p: bounds[0] < p[0] and p[0] < bounds[2] and bounds[1] < p[1] and p[1] < bounds[3]
 
     active_points = list(set(seeds)) # Remove duplicates, but use a list because random.choice require a sequence
     all_points = set(seeds)
@@ -60,9 +60,19 @@ def poision_disk_generator(inner_r, outer_r, seeds, bounds, k=10):
     for point in all_points:
         yield point
 
+    def check_dist(p, test_points, limit=inner_r):
+        """
+        Return true if any of the test_points within the limit distance of any of the test points
+        """
+        for test_point in test_points:
+            if dist(p, test_point) < limit:
+                # p is with in limit distance
+                return True
+        # p is not with in the limit distance of of the test points
+        return False
+
     while len(active_points) > 0:
         center = random.choice(active_points)
-        print("center: ", center) # Debug
         gen = firstn(k, offset_disk_generator(inner_r, outer_r, point))
         
         # Search for candidate point
@@ -71,18 +81,17 @@ def poision_disk_generator(inner_r, outer_r, seeds, bounds, k=10):
                     in_bounds,
                     gen)
             point = next(filter(
-                    lambda p: dist(p, center) < outer_r, # TODO check dist to all points
+                    lambda p: not check_dist(p, iter(all_points)),
                     bounded_gen))
 
+            assert not check_dist(point, all_points)
             assert in_bounds(point)
 
             # A new point was found
             active_points.append(point)
-            all_points.append(point)
+            all_points.add(point)
 
-            print(point) # Debug
             yield point
         except StopIteration:
             # No point was found, mark center as an inactive point
-            print("removed center: ", center)
             active_points.remove(center)
