f50a196b91d9391f49ba25f790359548b2026c0e
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2020-04-03 23:55:17 +0200

    [coord-along-edge] Add function to return the coordinate to a position along edge

--- utility.py
+++ utility.py
@@ -36,3 +36,51 @@ def distance(pos1: Tuple[float, float], pos2: Tuple[float, float]):
     x1, y1 = pos1
     x2, y2 = pos2
     return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def linear_equation(pos1: Tuple[float, float], pos2: Tuple[float, float]):
+    x_coords = [pos1[0], pos2[0]]
+    y_coords = [pos1[1], pos2[1]]
+    a, b = np.polyfit(x_coords, y_coords, 1)
+
+    def f(x):
+        return b + a * x
+
+    return f
+
+
+def position_on_edge(edge: sumolib.net.edge.Edge, pos: int):
+    """
+    :return: coordinate for pos meters from the start of the edge, following any shapes along edge
+    """
+    # Go through pair of coords, until meeting an edge, where if we travel through it, we have moved more than pos meters
+    remaining_distance = pos
+    for coord1, coord2 in (edge.getShape()[i:i + 2] for i in range(0, int(len(edge.getShape()) / 2 * 2), 2)):
+        if 0 < remaining_distance - distance(coord1, coord2):
+            remaining_distance -= distance(coord1, coord2)
+        else:
+            first_cord = coord1
+            second_cord = coord2
+            break
+
+    # Now we have the final edge where we need to find a location on.
+    # Find a linear equation from start to end of this stretch
+    f = linear_equation(first_cord, second_cord)
+
+    # Some nice bruteforcing ahead :)
+    # Potential coordinate for the correct position. Start at coord1.
+    pos_coords = (first_cord[0], first_cord[1])
+
+    # Move x one each iteration and calculate distance from this next position, and coord1.
+    # When we find a position that is larger than remaining distance, return the coordinate just before this.
+    while distance(first_cord, pos_coords) < remaining_distance:
+        # Go right
+        if (first_cord[0] < second_cord[0]):
+            pos_coords = (pos_coords[0] + 1, f(pos_coords[0] + 1))
+
+        # Go left
+        if (first_cord[0] > second_cord[0]):
+            pos_coords = (pos_coords[0] - 1, f(pos_coords[0] - 1))
+
+    # return position coords
+    return pos_coords[0], pos_coords[1]
