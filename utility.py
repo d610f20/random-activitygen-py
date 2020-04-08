6d607bb941108d0d077fc75f343b3c8776cca7f5
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2020-04-08 09:29:18 +0200

    [schools] Increase number of iterations in kmeans to get more consistent results when running the program

--- utility.py
+++ utility.py
@@ -44,7 +44,7 @@ def k_means_clusters(net: sumolib.net.Net, num_clusters: int):
     edges = net.getEdges()
 
     centroid_edges = [get_edge_pair_centroid(edge.getShape()) for edge in edges]
-    centroids = kmeans(centroid_edges, num_clusters, iter=5)
+    centroids = kmeans(centroid_edges, num_clusters, iter=25)
 
     clusters = [[] for x in range(num_clusters)]
     # Iterate through each edge, to decide which cluster it belongs to
