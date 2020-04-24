47f0ab1665952fd9f478c7511a24426081d63488
cogitantium <falke.cs@gmail.com>
2020-04-24 11:01:39 +0200

    [test-schools] Add TestInstance def

--- test_schools.py
+++ test_schools.py
@@ -1,4 +1,20 @@
-from test_city_gates import TestInstance
+from pathlib import Path
+from sys import stderr
+
+
+class TestInstance:
+    def __init__(self, name: str, gen_stats_file: str, real_stats_file: str):
+        self.name = name
+        self.gen_stats_file = gen_stats_file
+        self.real_stats_file = real_stats_file
+
+        try:
+            Path(self.gen_stats_file).resolve(strict=True)
+            Path(self.real_stats_file).resolve(strict=True)
+        except FileNotFoundError:
+            print(f"Files for test instance: {self.name} does not exist", file=stderr)
+            exit(1)
+
 
 test_instances = [
     TestInstance("Esbjerg", "out/cities/esbjerg.stat.xml", "stats/esbjerg.stat.xml"),
@@ -6,3 +22,10 @@ test_instances = [
 ]
 
 
+def main():
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
