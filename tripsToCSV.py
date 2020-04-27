4502e2fafa197a9ccac72c318e8ac11bdaa1b01c
NicEastvillage <nicoesterby@gmail.com>
2020-04-27 14:32:40 +0200

    [test] Add comments to tripsToCSV.py

--- tripsToCSV.py
+++ tripsToCSV.py
@@ -32,10 +32,12 @@ args = docopt(__doc__)
 net = sumolib.net.readNet(args["--net-file"])
 trips = ET.parse(args["--trips-file"])
 
+# Info about net size and edges
 offset_x, offset_y, xmax, ymax = net.getBoundary()
 net_width, net_height = xmax - offset_x, ymax - offset_y
 edge_count = len(net.getEdges())
 
+# base of file name, e.g. "vejen.trips.rou.xml" -> "vejen"
 fname = os.path.splitext(os.path.splitext(os.path.splitext(os.path.basename(args["--trips-file"]))[0])[0])[0]
 
 starts = []
@@ -50,9 +52,11 @@ with open(os.path.dirname(args["--trips-file"]) + f"/{fname}-trip-starts.csv", "
         progress = 0
         for edge in net.getEdges():
 
+            # Find all trips that starts and ends on this edge and save the position along the edge
             trip_starts = [float(trip.attrib["arrivalPos"]) for trip in trips.findall("trip") if trip.attrib["from"] == edge.getID()]
             trip_ends = [float(trip.attrib["departPos"]) for trip in trips.findall("trip") if trip.attrib["to"] == edge.getID()]
 
+            # Add trip start data points
             for start in trip_starts:
                 x, y = position_on_edge(edge, start)
                 x -= offset_x
@@ -61,6 +65,7 @@ with open(os.path.dirname(args["--trips-file"]) + f"/{fname}-trip-starts.csv", "
                 writer_starts.writerow(start)
                 starts.append(start)
 
+            # Add trip end data points
             for end in trip_ends:
                 x, y = position_on_edge(edge, end)
                 x -= offset_x
@@ -69,7 +74,15 @@ with open(os.path.dirname(args["--trips-file"]) + f"/{fname}-trip-starts.csv", "
                 writer_starts.writerow(end)
                 ends.append(end)
 
+            # Print a . whenever another 10% is done
+            progress += 1
+            if progress > next_print:
+                next_print += 0.1 * edge_count
+                print(".", end="")
+
+# Display the start and end positions
 if args["--display"]:
+    # Calculate dimensions and scaling
     max_size = 800
     width_height_relation = net_height / net_width
     if net_width > net_height:
@@ -81,6 +94,7 @@ if args["--display"]:
     width_scale = width / net_width
     height_scale = height / net_height
 
+    # Render start points
     img = Image.new("RGB", (width, height), (255, 255, 255))
     draw = ImageDraw.Draw(img, "RGBA")
     for point in starts:
@@ -92,6 +106,7 @@ if args["--display"]:
         draw.ellipse([x - r, y - r, x + r, y + r], fill=color)
     img.show()
 
+    # Render end points
     img = Image.new("RGB", (width, height), (255, 255, 255))
     draw = ImageDraw.Draw(img, "RGBA")
     for point in ends:
