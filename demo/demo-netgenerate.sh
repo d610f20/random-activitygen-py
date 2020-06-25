70bbfb7fd995ea1c80dea7f99ec76886a532754d
cogitantium <falke.cs@gmail.com>
2020-06-25 17:51:47 +0200

    [demo] Fix paths and names for netgenerate-flavoured demo script

--- demo/demo-netgenerate.sh
+++ demo/demo-netgenerate.sh
@@ -19,18 +19,18 @@ echo ""
 echo " RANDOMACTIVITYGEN "
 echo ""
 echo "====================="
-python ./randomActivityGen.py -n in/cities/esbjerg.net.xml -s in/cities/esbjerg.stat.xml -o out/cities/esbjerg.stat.xml --display --display.size 1500 --centre.pos 7474,1712
+python ./randomActivityGen.py -n in/example.net.xml -s in/example.stat.xml -o out/example.stat.xml --display --display.size 1500
 
 echo "====================="
 echo ""
 echo "    ACTIVITYGEN "
 echo ""
 echo "====================="
-podman run -it -v ~/projects/uni/d610f20/random-activitygen-py/:/rap sumo activitygen -n /rap/in/cities/esbjerg.net.xml -s /rap/out/cities/esbjerg.stat.xml -o /rap/out/cities/esbjerg.rou.xml
+podman run -it -v ~/projects/uni/d610f20/random-activitygen-py/:/rap sumo activitygen -n /rap/in/cities/esbjerg.net.xml -s /rap/out/example.stat.xml -o /rap/out/example.rou.xml
 
 echo "====================="
 echo ""
 echo "        SUMO "
 echo ""
 echo "====================="
-podman run -it -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/projects/uni/d610f20/random-activitygen-py/:/rap sumo-1.5 sumo-gui -n /rap/in/cities/esbjerg.net.xml -r /rap/out/cities/esbjerg.rou.xml --ignore-route-errors # -b 10000
+podman run -it -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/projects/uni/d610f20/random-activitygen-py/:/rap sumo-1.5 sumo-gui -n/rap/in/example.net.xml -r /rap/out/example.rou.xml --ignore-route-errors # -b 10000
